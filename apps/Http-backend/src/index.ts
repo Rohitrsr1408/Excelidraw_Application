767c10568136178c3297f703f35892eae2f8e6a7
Rohtirsr1408 <rohitrana8041@gmail.com>
2025-08-25 18:28:57 +0530

    steps10-12

--- apps/Http-backend/src/index.ts
+++ apps/Http-backend/src/index.ts
@@ -1,4 +1,16 @@
 import express from "express";
-
+import jwt from "jsonwebtoken";
+import { JWTSECRET } from "./config";
+import { middleware } from "./middleware";
 const app = express();
-app.listen(5000);
+
+app.post("/signup", (req, res) => {});
+
+app.post("/signin", (req, res) => {
+  let userid = 1;
+  const token = jwt.sign({ userid }, JWTSECRET);
+
+  res.json({ token });
+});
+
+app.post("/room", middleware, (req, res) => {});
